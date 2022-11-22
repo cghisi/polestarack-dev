940588b36d8901c0e914d3e867a01faacbf45182
Christophe Ghisi <christophe.ghisi@gmail.com>
2022-11-22 23:51:58 +0000

    Update Layout.js and manifest.json

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,8 @@ const Layout = ({ children, navigation, settings }) => (
       <link rel="manifest" href="/manifest.json" />
       <link rel="icon" href="/favicon_32x32.png" type="image/png" />
       <meta name="theme-color" content="#90cdf4" />
+      <link rel="apple-touch-icon" href="/favicon_64x64.png" />
+      <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
       <title>
         Polestar Hack - Recreate the Polestar car website experience using
         NextJS and Prismic
