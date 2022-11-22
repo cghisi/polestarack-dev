111018ef4a1ed8cd3e7af2141f28b8a2d5587110
Christophe Ghisi <christophe.ghisi@gmail.com>
2022-11-22 23:22:50 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,13 +8,16 @@ const Layout = ({ children, navigation, settings }) => (
   <>
     <Head>
       <meta charSet="utf-8" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon_32x32.png" type="image/png" />
+      <meta name="theme-color" content="#90cdf4" />
       <title>
         Polestar Hack - Recreate the Polestar car website experience using
         NextJS and Prismic
       </title>
       <meta
         name="description"
-        content="Your consulting team that will help you with your website, translations..."
+        content="Recreate the Polestar car website experience using NextJS and Prismic"
       />
     </Head>
     <Header navigation={navigation}></Header>
